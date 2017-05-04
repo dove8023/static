9c02e76bae578cc52187b79aac3d6842e1ff8aac
hexisen <616082046@qq.com>
2017-05-04 16:06:45 +0800

    增加自动部署功能

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express=require("express");
 const http=require("http");
+const child = require("child_process");
 
 let app = express();
 app.use(express.static("./www"));
+app.post("/update", (req, res)=>{
+    child.exec("git pull", (error, stdout, stderr)=>{
+        if(error){
+            console.log(error);
+        }
+
+        if(stdout){
+            console.log(stdout);
+            res.send(stdout);
+            return;
+        }
+
+        if(stderr){
+            console.log(stderr);
+        }
+        res.send(error || stderr);
+    });
+});
 
 let server = http.createServer(app);
 server.listen(3000, ()=>{
