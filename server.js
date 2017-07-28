761418272a0557a7069963911c44e70b960c5b1c
hexisen <616082046@qq.com>
2017-07-28 10:18:23 +0800

    change

--- server.js
+++ server.js
@@ -60,13 +60,13 @@ app.post("/update", (req, res)=>{
         return;
     }
 
-    /*child.exec("git pull", (error, stdout, stderr)=>{
+    child.exec("git pull", (error, stdout, stderr)=>{
         res.json({
             error : error,
             stdout: stdout,
             stderr: stderr
         })
-    });*/
+    });
 });
 
 
