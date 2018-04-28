1e812bef403693213279afcee8c7b941fc839c09
hexisen <616082046@qq.com>
2018-04-28 11:15:18 +0800

    koa simple static server.

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 let koa = require("koa");
+let static = require("koa-static");
 let app = new koa();
 
 
-app.use(function *(next){
+app.use(function* (next) {
     var start = new Date;
     yield next;
     var ms = new Date - start;
     this.set('X-Response-Time', ms + 'ms');
 });
 
-app.use(function *(next){
+app.use(function* (next) {
     var start = new Date;
     yield next;
     var ms = new Date - start;
     console.log("%s %s - %s", this.method, this.url, ms);
 });
 
-
-app.use(function *(){
-    this.body = "<h1>Hello world</h1>";
-});
-
-// app.listen(3000);
+app.use(static("./www"));
 
 const http = require("http");
 
